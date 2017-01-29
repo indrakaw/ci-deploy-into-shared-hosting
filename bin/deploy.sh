c80049f46d73d3e322408414fa831b3914cddfc5
Indra Kurniawan <indrakaw@teknik.io>
2017-01-29 04:39:32 -0500

    lftp test (a4)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,2 @@
 # Setup
-BASE_DIR="."
-echo $BASE_DIR
-
-lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R $BASE_DIR /"
+lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R . public_html"
