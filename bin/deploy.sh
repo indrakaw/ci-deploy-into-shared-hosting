d448cecd282ff4b996ecaefaa71ee725d3df2ef4
Indra Kurniawan <indrakaw@teknik.io>
2017-01-29 04:26:27 -0500

    lftp test (a1)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,3 @@
 # Setup
-lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R ./* public_html/"
+BASE_DIR=$(cd $(dirname "$0") && pwd)
+lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R $BASE_DIR public_html/"
